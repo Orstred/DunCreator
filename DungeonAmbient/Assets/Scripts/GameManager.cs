9394b474bb9b69b487a9df7594fe1bc5cf8f8c17
Guits <guilhermessecoortiz@gmail.com>
2022-09-12 15:26:40 +0100

    Project init & Build system

---
+++ DungeonAmbient/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    #region Singleton
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+            Destroy(this);
+    }
+    #endregion
+
+}
