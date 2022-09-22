3dbc619a132f35ab09fbc9726ce95dc1195ac6b3
Guits <guilhermessecoortiz@gmail.com>
2022-09-22 18:28:48 +0100

    FinishedGrid

---
+++ DungeonAmbient/Assets/Scripts/GridBlock.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBlock : MonoBehaviour
+{
+    public GridBlock UpperNeighbour;
+    public GridBlock LowerNeighbour;
+    public GridBlock LeftNeighbour; 
+    public GridBlock RightNeighbour;
+}
